38f05ac9d1eea831a8dc0f84caef27a30213aece
Heggy Castaneda <heggyy@gmail.com>
2021-07-20 09:59:49 -0400

    finalize

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import JokeApp from './components/JokeApp';
 function App() {
   return (
     <div className="App">
+      <h1>Chuck Norris Joke Generator</h1>
       {/* Joke app go here */}
       <JokeApp />
     </div>
--- src/components/JokeApp.js
+++ src/components/JokeApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import spinnerBubble from "./spinnerBubble.gif";
 
 class JokeApp extends Component {
   constructor(props) {
@@ -6,29 +7,54 @@ class JokeApp extends Component {
 
     this.state = {
       joke: "",
+      isLoading: false,
     };
   }
 
   render() {
     return (
       <>
-        <h1>{this.state.joke}</h1>
+        <hr />
+        <h2>
+          {this.state.isLoading ? (
+            <img src={spinnerBubble} alt="loading" width="150"/>
+          ) : (
+            this.state.joke
+          )}
+        </h2>
         <button onClick={this._fetchJoke}>Get joke</button>
       </>
     );
   }
 
   _fetchJoke = () => {
-    const jokeUrl = "https://api.chucknorris.io/jokes/search?query=california";
+    this.setState(
+      {
+        isLoading: true,
+      },
+      () => {
+        // inside cb after set isLoading: true
+        const jokeUrl =
+          "https://api.chucknorris.io/jokes/search?query=california";
 
-    fetch(jokeUrl)
-      .then((response) => response.json())
-      .then((jokeJson) => {
-        console.log(jokeJson.result[0].value);
-        this.setState({
-          joke: jokeJson.result[0].value,
-        });
-      });
+        fetch(jokeUrl)
+          .then((response) => response.json())
+          .then((jokeJson) => {
+            const maxNumJokes = jokeJson.total;
+            const randomJoke = Math.floor(Math.random() * maxNumJokes); // random num range [0, 11)
+            console.log(jokeJson.result[randomJoke].value);
+            this.setState(
+              {
+                joke: jokeJson.result[randomJoke].value,
+                isLoading: false,
+              },
+              () => {
+                console.log("New joke stored");
+              }
+            );
+          });
+      }
+    );
   };
 }
 
